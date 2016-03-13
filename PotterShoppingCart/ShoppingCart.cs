1bef301d1b4b0c16a4ac36bb4dd9a65ab1535b54
sam lin <evaoosyz@gmail.com>
2016-03-13 12:40:22 +0800

    綠燈 一二三集各買一本_價格應為270

--- PotterShoppingCart/ShoppingCart.cs
+++ PotterShoppingCart/ShoppingCart.cs
@@ -13,28 +13,40 @@ namespace PotterShoppingCart
             int result = 0;
             int thePhilosophersStoneCount = 0;
             int theChamberofSecretsCount = 0;
-  
+            int thePrisonerofAzkaban = 0;
+             
             foreach (var item in bookList)
             {
                 //算買哪些是活動書
                 switch (item.ISBN)
                 {
                     case 9573317249://哈利波特 : 神秘的魔法石 
-                    thePhilosophersStoneCount++;
+                        thePhilosophersStoneCount++;
                         break;
                     case 9573317583://哈利波特 : 消失的密室
-                    theChamberofSecretsCount++;
-                        break; 
+                        theChamberofSecretsCount++;
+                        break;
+                    case 9573318008://哈利波特 : 消失的密室
+                        thePrisonerofAzkaban++;
+                        break;
                 }
 
                 //原價
-                result +=item.Price;
+                result += item.Price;
             }
-            
+
             //折扣條件
-            if (thePhilosophersStoneCount > 0 && theChamberofSecretsCount > 0)
+            if (thePhilosophersStoneCount > 0 
+                && theChamberofSecretsCount > 0
+                && thePrisonerofAzkaban == 0)
             {
                 result = (int)(result * 0.95);
+            }
+            else if(thePhilosophersStoneCount > 0 
+                    && theChamberofSecretsCount > 0
+                    && thePrisonerofAzkaban > 0)
+            {
+                result = (int)(result * 0.9);
             }
 
 
